f9f04bacf7ca33c23a48d69c4165326a759588f4
Afifah Khairunnisa Nugroho <133479783+afifahkhrnnisa@users.noreply.github.com>
2023-12-06 11:14:51 +0800

    Add files via upload

---
+++ registrasi.php
@@ -0,0 +1,29 @@
+<?php
+include "koneksi.php";
+if (!$koneksi) { 
+    die("Koneksi gagal: " . mysqli_connect_error());
+}
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $nik = $_POST["nik"];
+    $email = $_POST["email"];
+    $password = $_POST["password"] ;
+
+    $sqli = "INSERT INTO user (nik, email, password) VALUES ('$nik', '$email', '$password')";
+   
+    if (empty($nik) || empty($email) || empty($password)) {
+        echo "Isi dengan lengkap!";
+    } 
+    else {
+        $sqli = "INSERT INTO user (nik, email, password) VALUES ('$nik', '$email', '$password')";
+       
+        if(mysqli_query($koneksi, $sqli)) {
+            header("Location: login.html");
+        } 
+        else {
+            echo "Error: " . $sqli ."". mysqli_error($koneksi);
+        }
+    }
+}
+
+?>
