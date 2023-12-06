166dde233696e7e3b1b5a296e84ea1ea3624e35e
Afifah Khairunnisa Nugroho <133479783+afifahkhrnnisa@users.noreply.github.com>
2023-12-06 21:20:12 +0800

    Update registrasi.php

--- registrasi.php
+++ registrasi.php
@@ -1,3 +1,37 @@
+<?php
+include "koneksi.php";
+if (!$koneksi) { 
+    die("Koneksi gagal: " . mysqli_connect_error());
+}
+
+if (isset($_POST["regis"])) {
+    $nik = $_POST["nik"];
+    $email = $_POST["email"];
+    $password = $_POST["password"];
+
+    if (empty($nik) || empty($email) || empty($password)) {
+        echo "<script>alert('Isi dengan lengkap!'); </script>";
+    } 
+    else {
+        $periksa = "SELECT * FROM user WHERE nik='$nik'";
+        $result = mysqli_query($koneksi, $periksa);
+        if (mysqli_num_rows($result) > 0) {
+            echo "<script>alert('NIK sudah terdaftar. Silahkan gunakan NIK lain.'); </script>";
+        } 
+        else {
+            $sqli = "INSERT INTO user (nik, email, password) VALUES ('$nik', '$email', '$password')";
+            
+            if(mysqli_query($koneksi, $sqli)) {
+                header("Location: login.php");
+            } 
+            else {
+                echo "Error: " . $sqli . " " . mysqli_error($koneksi);
+            }
+        }
+    }
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -17,7 +51,7 @@
         <br>
         <input type="password" placeholder="Masukan Password" name="password">
         <br>
-        <input type="submit" class="button" value="Registrasi">
+        <input type="submit" class="button" value="Registrasi" name="regis">
         <br>
       </form>
     </div>
