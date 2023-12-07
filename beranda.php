9d56b64a84baf1d508a5aac3bd2f733941f25d0a
Afifah Khairunnisa Nugroho <133479783+afifahkhrnnisa@users.noreply.github.com>
2023-12-07 17:35:08 +0800

    Update beranda.php

--- beranda.php
+++ beranda.php
@@ -20,7 +20,7 @@ if(isset($_SESSION["login"])){
             border-radius: 20px; 
       }
     </style>
-    <title>dashboard</title>
+    <title>beranda</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" 
     integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 </head>
